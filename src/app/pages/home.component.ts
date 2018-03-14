e8e2c0094ade76b8e137534bd270cd7241b6f790
choihjin <chlgurwls137@gmail.com>
2018-03-14 17:20:02 +0900

    add bootstrap

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<div class="home">Home</div>`,
+  template: `<div class="home">Home</div>
+  <input><button class="btn">df</button>`,
   styles: [`.home { font-size: 2em; padding: 60px; }`]
 })
 export class HomeComponent { }
